5b688b6f0579c6f59e747185038fad79310bfe7c
Carlos Vicente <carlosvicente.alu@iespacomolla.es>
2021-03-26 12:23:50 +0100

    Creacion Funciones en el controlador de Sign In

--- controllers/signinController.php
+++ controllers/signinController.php
@@ -1,11 +1,17 @@
 <?php
 
+function comprobarDatos() {
+    echo 'Funciona1';
+    crearUsuario();
+}
+
+function crearUsuario() {
+    echo 'Funciona2';
+}
 
 
-if (!empty($_POST['nombre']) && !empty($_POST['nick']) && 
-!empty($_POST['contrasena']) && !empty($_POST['correo']) && 
-!empty($_POST['telefono']) && !empty($_POST['plan'])) {
-    echo 'Funciona';
+if (!empty($_POST['nombre']) && !empty($_POST['nick']) && !empty($_POST['contrasena']) && !empty($_POST['correo']) && !empty($_POST['telefono']) && !empty($_POST['plan'])) {
+    comprobarDatos();
 } else {
     echo 'Pagina de error';
 }
