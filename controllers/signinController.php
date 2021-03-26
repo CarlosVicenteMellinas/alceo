0c948acf3b3e841334cb6bad1a98d90ea5b06686
Carlos Vicente <carlosvicente.alu@iespacomolla.es>
2021-03-26 12:17:22 +0100

    Depuracion codigo

--- controllers/signinController.php
+++ controllers/signinController.php
@@ -2,7 +2,9 @@
 
 
 
-if (!empty($_POST['nombre']) && !empty($_POST['nick']) && !empty($_POST['contrasena']) && !empty($_POST['correo']) && !empty($_POST['telefono']) && !empty($_POST['plan'])) {
+if (!empty($_POST['nombre']) && !empty($_POST['nick']) && 
+!empty($_POST['contrasena']) && !empty($_POST['correo']) && 
+!empty($_POST['telefono'])) {
     echo 'Funciona';
 } else {
     echo 'Pagina de error';
