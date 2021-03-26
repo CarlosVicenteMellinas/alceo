98aa993e21b7733e8d824332025213199478798c
Carlos Vicente <carlosvicente.alu@iespacomolla.es>
2021-03-26 12:54:19 +0100

    Formulario controlador Sign In

--- controllers/signinController.php
+++ controllers/signinController.php
@@ -1,12 +1,14 @@
 <?php
 
 function comprobarDatos() {
-    echo 'Funciona1';
     crearUsuario();
 }
 
 function crearUsuario() {
-    echo 'Funciona2';
+    $link = mysqli_connect('172.18.0.2', 'dbAdmin', 'C0nTr@s3ñ4', 'AlceoBD');
+    $query = mysqli_query($link, 'INSERT INTO USUARIO (nombre, nickname, contrasenya, correo, telefono, plan) VALUES 
+    ("'.$_POST["nombre"].'", "'.$_POST["nick"].'", "'.$_POST["contrasena"].'", "'.$_POST["correo"].'", "'.$_POST["telefono"].'", '.$_POST["plan"].')');
+    mysqli_close($link);
 }
 
 
