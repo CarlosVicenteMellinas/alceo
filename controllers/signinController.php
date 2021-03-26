f275a19f64ea560675e14ad6bfb36a44a5fa1ddf
Carlos Vicente <carlosvicente.alu@iespacomolla.es>
2021-03-26 12:41:47 +0100

    Depuracion de codigo

--- controllers/signinController.php
+++ controllers/signinController.php
@@ -10,13 +10,8 @@ function crearUsuario() {
 }
 
 
-if (!empty($_POST['nombre']) && !empty($_POST['nick']) && !empty($_POST['contrasena']) && !empty($_POST['correo']) && !empty($_POST['telefono'])) {
-    if (!empty($_POST['plan'])) {
-        comprobarDatos();
-    } else {
-        echo $_POST['plan'];
-        echo 'Error';
-    }
+if (!empty($_POST['nombre']) && !empty($_POST['nick']) && !empty($_POST['contrasena']) && !empty($_POST['correo']) && !empty($_POST['telefono']) && !empty($_POST['plan'])) {
+    comprobarDatos();
 } else {
     echo 'Pagina de error';
 }
