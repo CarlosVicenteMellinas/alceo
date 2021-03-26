709b76cc3e0c23f9a36f9c692a4079852248f5e1
Carlos Vicente <carlosvicente.alu@iespacomolla.es>
2021-03-26 12:15:04 +0100

    Controlador Sign in 2

--- controllers/signinController.php
+++ controllers/signinController.php
@@ -2,7 +2,7 @@
 
 
 
-if (isset($_POST['nombre'])) {
+if (!empty($_POST['nombre']) && !empty($_POST['nick']) && !empty($_POST['contrasena']) && !empty($_POST['correo']) && !empty($_POST['telefono']) && !empty($_POST['plan'])) {
     echo 'Funciona';
 } else {
     echo 'Pagina de error';
