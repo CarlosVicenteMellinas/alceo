b6226c87ebd977d559de2d000cb26d9f67f1d05c
Carlos Vicente <carlosvicente.alu@iespacomolla.es>
2021-03-29 13:10:42 +0200

    Depuracion de codigo

--- controllers/ejercicioController.php
+++ controllers/ejercicioController.php
@@ -38,22 +38,22 @@ function insertar() {
         $query = mysqli_query($link, 'INSERT INTO EJERCICIO (nombre, dificultad, foto, video) VALUES 
         ("'.$_POST["nombre"].'", '.$_POST["dificultad"].', "'.$_POST["foto"].'", "'.$_POST["video"].'")');
         mysqli_close($link);
-        header("Location: ../paginas/area-usuario.html");
+        header("Location: ../admin/ejercicios.php");
     } else if (!empty($_POST['foto'])) {
         $query = mysqli_query($link, 'INSERT INTO EJERCICIO (nombre, dificultad, foto) VALUES 
         ("'.$_POST["nombre"].'", '.$_POST["dificultad"].', "'.$_POST["foto"].'")');
         mysqli_close($link);
-        header("Location: ../paginas/area-usuario.html");
+        header("Location: ../admin/ejercicios.php");
     } else if (!empty($_POST['video'])) { 
         $query = mysqli_query($link, 'INSERT INTO EJERCICIO (nombre, dificultad, video) VALUES 
         ("'.$_POST["nombre"].'", '.$_POST["dificultad"].', "'.$_POST["video"].'")');
         mysqli_close($link);
-        header("Location: ../paginas/area-usuario.html");
+        header("Location: ../admin/ejercicios.php");
     } else {
         $query = mysqli_query($link, 'INSERT INTO EJERCICIO (nombre, dificultad) VALUES 
         ("'.$_POST["nombre"].'", '.$_POST["dificultad"].')');
         mysqli_close($link);
-        header("Location: ../paginas/area-usuario.html");
+        header("Location: ../admin/ejercicios.php");
     }
 }
 
