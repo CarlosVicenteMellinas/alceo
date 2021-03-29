742c5e23ddb8058f680c48022114d31d6a1c1625
Carlos Vicente <carlosvicente.alu@iespacomolla.es>
2021-03-29 11:09:20 +0200

    Validacion usuario mediante php

--- controllers/loginController.php
+++ controllers/loginController.php
@@ -1,7 +1,31 @@
 <?php
 
+function limpiarDatos($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+
 function comprobarCredenciales() {
-    iniciarSesion();
+    $link = mysqli_connect('172.18.0.2', 'dbAdmin', 'C0nTr@s3ñ4', 'AlceoBD');
+    $usuario = limpiarDatos($_POST['loginNombre']);
+    $contrasena = limpiarDatos($_POST['loginContrasena']);
+    $valido = true;
+
+    $query = mysqli_query($link, 'SELECT * FROM USUARIO WHERE nickname="'.$usuario.'" ');
+    $results = mysqli_fetch_array($query);
+    if ($results > 0) {
+        mysqli_close($link);
+        $valido = false;
+        $usuarioError = 'El usuario no existe';
+        include '../paginas/area-usuario.php';
+    }
+    mysqli_free_result($query);
+
+    if ($valido) {
+        iniciarSesion();
+    }
 }
 
 function iniciarSesion() {
