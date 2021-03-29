a6627e6620089ae4dda5be3bcfe4eafb7fa5ee04
Carlos Vicente <carlosvicente.alu@iespacomolla.es>
2021-03-29 09:53:50 +0200

    depuracion de codigo nº2456

--- controllers/signinController.php
+++ controllers/signinController.php
@@ -7,7 +7,7 @@ function validarNick($link) {
         $nickError = 'El nick no puede tener una longitud superior a 20 caracteres';
         include '../paginas/signin.php';
     }
-    $query = mysqli_query($link, 'SELECT * FROM USUARIOS WHERE nickname='.$nick.' ');
+    $query = mysqli_query($link, 'SELECT * FROM USUARIOS WHERE nickname="'.$nick.'" ');
     $results = mysqli_num_rows($query);
     if ($results > 0) {
         mysqli_close($link);
