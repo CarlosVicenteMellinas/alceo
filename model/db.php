b625e9b2e9d092a7a9bb9ac2fe40d5e146c6a928
Carlos Vicente <carlosvicente.alu@iespacomolla.es>
2021-03-30 16:02:44 +0200

    Comentando las direcciones

--- model/db.php
+++ model/db.php
@@ -3,11 +3,14 @@ class Conectar{
 
     public static function conexion(){
         /*
+        //Dirección local
         $direccion = "localhost";
         $usuario = "root";
         $contrasenya = "";
         $bd = "AlceoBD";
         */
+
+        //Dirección global
         $direccion = "172.18.0.2";
         $usuario = "dbAdmin";
         $contrasenya = "C0nTr@s3ñ4";
