f198765aecbd177787dca3a48cf4de63e5a44b5f
Carlos Vicente <carlosvicente.alu@iespacomolla.es>
2021-03-30 11:04:05 +0200

    Añadiendo la direccion de la bd en local y en la pag

--- model/db.php
+++ model/db.php
@@ -2,11 +2,17 @@
 class Conectar{
 
     public static function conexion(){
+        
         $direccion = "localhost";
         $usuario = "root";
         $contrasenya = "";
         $bd = "AlceoBD";
-
+        /*
+        $direccion = "172.18.0.2";
+        $usuario = "dbAdmin";
+        $contrasenya = "C0nTr@s3ñ4";
+        $bd = "AlceoBD";
+        */
         $conexion=new mysqli($direccion, $usuario, $contrasenya, $bd);
         return $conexion;
     }
