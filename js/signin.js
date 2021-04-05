1f65e3b485fd78eaaff7ad129f27208e79b6896a
Carlos Vicente <carlosvicente.alu@iespacomolla.es>
2021-04-05 19:39:11 +0200

    Cambio direccion bd a global

--- js/signin.js
+++ js/signin.js
@@ -18,20 +18,45 @@ function insertarError(input, texto) {
 function eliminarError(input) {
     let error = document.querySelector(`#${input.id} + .error`);
     if (error) {
-        console.log(error);
         error.parentNode.removeChild(error);
     } 
 }
 
 function comprobarNick(nick) {
-    if (nick.value.length < 20) {
-        nick.setCustomValidity('');
-        eliminarError(nick);
-    } else {
+    nick.setCustomValidity('');
+    eliminarError(nick);
+
+    if (nick.value.length > 20) {
         let texto = 'El nombre de usuario no puede tener una longitud superior a 20 caracteres'
         nick.setCustomValidity(texto);
         insertarError(nick, texto);
     }
+}
+
+function comprobarContrasena(contrasena) {
+    contrasena.setCustomValidity('');
+    eliminarError(contrasena);
+
+    if (contrasena.value.length < 8) {
+        let texto = 'La contraseña debe de tener una longitud superior a 8 caracteres';
+        contrasena.setCustomValidity(texto);
+        insertarError(contrasena, texto);
+    } else if (contrasena.value.length > 200) {
+        let texto = 'La contraseña no puede tener una longitud superior a 200 caracteres';
+        contrasena.setCustomValidity(texto);
+        insertarError(contrasena, texto);
+    }  else if (!/[a-z]/.test(contrasena.value)) {
+        let texto = 'La contraseña debe de tener al menos una letra minúscula';
+        contrasena.setCustomValidity(texto);
+        insertarError(contrasena, texto);
+    }  else if (!/[A-Z]/.test(contrasena.value)) {
+        let texto = 'La contraseña debe de tener al menos una letra mayúscula';
+        insertarError(contrasena, texto);
+    }  else if (!/[0-9]/.test(contrasena.value)) {
+        let texto = 'La contraseña debe de tener al menos un caracter numérico';
+        contrasena.setCustomValidity(texto);
+        insertarError(contrasena, texto);
+    } 
 }
 
 function main() {
@@ -46,7 +71,8 @@ function main() {
     boton = document.getElementById("enviar");
 
     nick.onchange = (ev) => comprobarNick(ev.target);
-    
+    contrasena.onchange = (ev) => comprobarContrasena(ev.target);
+
 }
 
 main();
